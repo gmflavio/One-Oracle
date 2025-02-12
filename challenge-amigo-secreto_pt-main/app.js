6c5b4a1d79b2986bce42803e4d7fd9df2ab2b7d4
gmflavio <flavio.gmrj@gmail.com>
2025-02-12 19:57:31 -0300

    amigo.secreto

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,41 @@
+let amigos = [];
+
+document.querySelector('.button-add').addEventListener('click', () => {
+    
+    const nomeInput = document.getElementById('amigo');
+    const nome = nomeInput.value.trim();
+    
+    if (nome === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+    amigos.push(nome);
+    
+    nomeInput.value = ''; // Limpando a entrada
+    
+    atualizarLista();
+});
+
+function atualizarLista() {
+    const listaNomes = document.getElementById('listaAmigos');
+    listaNomes.innerHTML = '';
+    amigos.forEach((nome) => {
+        const li = document.createElement('li');
+        li.textContent = nome;
+        listaNomes.appendChild(li);
+    });
+}
+
+document.querySelector('.button-draw').addEventListener('click', () => {
+    
+    if (amigos.length === 0) {
+        alert('A lista está vazia.');
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    
+    const nomeSorteado = amigos[indiceAleatorio];
+    
+    document.getElementById('resultado').innerHTML = `<li>Amigo Secreto: ${nomeSorteado}</li>`;
+});
